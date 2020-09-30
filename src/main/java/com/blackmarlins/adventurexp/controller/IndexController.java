757e1c3d9df9de3505274f5387bdc19eab144f29
Cristian <cristi.valentin29@gmail.com>
2020-09-30 13:08:01 +0200

    Project Setup
    Add the initial files of the Springboot Project

---
+++ src/main/java/com/blackmarlins/adventurexp/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.blackmarlins.adventurexp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController
+{
+    // First mapping to be called when accessing the base url. It determines where to go next
+    @RequestMapping(value = {"", "/", "/index", "/index/"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String index()
+    {
+        return "index";
+    }
+}
