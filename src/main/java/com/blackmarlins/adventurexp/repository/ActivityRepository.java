577953b331893cad287e6ce9b22acb4818ea7fc0
Alex <a.sandrovschii@gmail.com>
2020-10-02 13:58:19 +0200

    Activity REPO

--- src/main/java/com/blackmarlins/adventurexp/repository/ActivityRepository.java
+++ src/main/java/com/blackmarlins/adventurexp/repository/ActivityRepository.java
@@ -3,6 +3,5 @@ package com.blackmarlins.adventurexp.repository;
 import com.blackmarlins.adventurexp.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ActivityRepository extends JpaRepository <Activity, Long> {
-
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
 }
